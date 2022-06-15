71fd554c1dd4e20c8b4785ab93a886a554fa68cd
Grzegorz Oleksa <grzesieq92@gmail.com>
2022-06-15 09:57:49 +0200

    calculateResult function added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import Form from "./Form";
 import Footer from "./Footer";
 import Container from "./Container";
+import { useState } from "react";
+import { currencies } from "./Form/index.js";
 
 function App() {
+  const [result, setResult] = useState();
+
+  const calculateResult = (currency, amount) => 
+  {const rate = currencies.find(({ short }) => short === currency).rate;
+    setResult({
+      targetAmount: amount / rate,
+      currency,
+    });
+  };
+
   return (
     <div className="App">
       <Container>
-        <Form />
+        <Form 
+        result={result}
+        calculateResult={calculateResult}
+        />
       </Container>
       <Footer />
     </div>
