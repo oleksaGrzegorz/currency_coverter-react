d30eabd06f3b62b497f9d21f3822d8027fcd3891
Grzegorz Oleksa <grzesieq92@gmail.com>
2022-06-15 10:00:41 +0200

    Footer component trimed

--- src/Footer/index.js
+++ src/Footer/index.js
@@ -1,31 +1,31 @@
 import "./style.css";
 
 const Footer = () => (
-    <footer className="footer">
-        <div className="footer__container">
-            <p className="footer__text">
-                <a className=
-                    "footer__link"
-                    target="_blank"
-                    rel="noreferrer"
-                    href="https://icons8.com/icon/42279/money"
-                >
-                    "Money" icon
-                </a>
-                
-                <span> from </span>
+  <footer className="footer">
+    <div className="footer__container">
+      <p className="footer__text">
+        <a
+          className="footer__link"
+          target="_blank"
+          rel="noreferrer"
+          href="https://icons8.com/icon/42279/money"
+        >
+          "Money" icon
+        </a>
 
-                <a className=
-                    "footer__link"
-                    target="_blank"
-                    rel="noreferrer"
-                    href="https://icons8.com"
-                >
-                    Icons8.com
-                </a>
-            </p>
-        </div>
-    </footer>
+        <span> from </span>
+
+        <a
+          className="footer__link"
+          target="_blank"
+          rel="noreferrer"
+          href="https://icons8.com"
+        >
+          Icons8.com
+        </a>
+      </p>
+    </div>
+  </footer>
 );
 
 export default Footer;
