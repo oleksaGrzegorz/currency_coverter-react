ab22279a40a59b416c1a443da6745267053b96d7
Grzegorz Oleksa <grzesieq92@gmail.com>
2022-09-16 09:55:39 +0200

    component clock added

--- src/Clock/index.js
+++ src/Clock/index.js
@@ -1,10 +1,32 @@
+import { useEffect, useState } from "react";
 import "./style.css";
 
+const Clock = () => {
+  const [date, setDate] = useState(new Date());
 
-const Clock = () => (
-  <div className="clock__container">
-    <p>Czas i data to:</p>
-  </div>
-);
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setDate(new Date());
+    }, 1000);
+
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, []);
+
+  return (
+    <div className="clock__container">
+      {date.toLocaleString(undefined, {
+        weekday: "long",
+        hour: "2-digit",
+        minute: "2-digit",
+        second: "2-digit",
+        day: "numeric",
+        month: "long",
+        year: "numeric",
+      })}
+    </div>
+  );
+};
 
 export default Clock;
