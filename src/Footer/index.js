30c4d6b4af8d933fcab497a902b57f4ff0ce8655
Grzegorz Oleksa <grzesieq92@gmail.com>
2022-09-18 10:01:32 +0200

    space beetween word added

--- src/Footer/index.js
+++ src/Footer/index.js
@@ -3,25 +3,23 @@ import "./style.css";
 const Footer = () => (
   <footer className="footer">
     <div className="footer__container">
-        <a
-          className="footer__link"
-          target="_blank"
-          rel="noreferrer"
-          href="https://icons8.com/icon/42279/money"
-        >
-          "Money" icon
-        </a>
+      <a
+        className="footer__link"
+        target="_blank"
+        rel="noreferrer"
+        href="https://icons8.com/icon/42279/money"
+      >
+        "Money" icon from:{" "}
+      </a>
 
-        <span> from </span>
-
-        <a
-          className="footer__link"
-          target="_blank"
-          rel="noreferrer"
-          href="https://icons8.com"
-        >
-          Icons8.com
-        </a>
+      <a
+        className="footer__link"
+        target="_blank"
+        rel="noreferrer"
+        href="https://icons8.com"
+      >
+        Icons8.com
+      </a>
     </div>
   </footer>
 );
