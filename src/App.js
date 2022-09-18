2499e79aa564bcd4d87181592015d7aa0aea6ddd
Grzegorz Oleksa <grzesieq92@gmail.com>
2022-09-18 10:02:04 +0200

    empty space removed

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ import currencies from "./currencies";
 
 function App() {
   const [result, setResult] = useState();
-
   const calculateResult = (currency, amount) => {
     const rate = currencies.find(({ short }) => short === currency).rate;
     setResult({
