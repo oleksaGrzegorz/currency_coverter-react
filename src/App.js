ad89274742fb2e6ad8748b170c8199ae46e982dd
Grzegorz Oleksa <grzesieq92@gmail.com>
2023-03-08 13:16:41 +0100

    function from App.js deleted

--- src/App.js
+++ src/App.js
@@ -2,24 +2,13 @@ import Form from "./Form";
 import Footer from "./Footer";
 import Container from "./Container";
 import Clock from "./Clock";
-import { useState } from "react";
-import currencies from "./currencies";
 
 function App() {
-  const [result, setResult] = useState();
-  const calculateResult = (currency, amount) => {
-    const rate = currencies.find(({ short }) => short === currency).rate;
-    setResult({
-      targetAmount: amount / rate,
-      currency,
-    });
-  };
-
   return (
     <div className="App">
       <Clock />
       <Container>
-        <Form result={result} calculateResult={calculateResult} />
+        <Form />
       </Container>
       <Footer />
     </div>
