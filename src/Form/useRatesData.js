8f0ef959cd27b80336bb18eb9404738d4fe1f02b
Grzegorz Oleksa <grzesieq92@gmail.com>
2023-03-13 15:28:12 +0100

    customHook changed-adding status loading and error

--- src/Form/useRatesData.js
+++ src/Form/useRatesData.js
@@ -1,17 +1,32 @@
 import { useState, useEffect } from "react";
 
 const useRatesData = () => {
-  const [ratesData, setRatesData] = useState();
+  const [ratesData, setRatesData] = useState({
+    state: "loading",
+  });
 
   const API_URL = "https://api.exchangerate.host/latest?base=PLN";
 
   useEffect(() => {
     fetch(API_URL)
-      .then((response) => response.json())
-      .then((data) => {
-        setRatesData(data.rates);
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Error fetching data");
+        }
+        return response.json();
       })
-      .catch((error) => console.error(error));
+      .then((data) => {
+        setRatesData({
+          state: "success",
+          rates: data.rates,
+          date: data.date,
+        });
+      })
+      .catch(() => {
+        setRatesData({
+          state: "error",
+        });
+      });
   }, []);
 
   return ratesData;
